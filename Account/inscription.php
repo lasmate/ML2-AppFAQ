649e13c39406cc9edeafedbe3c50be556ede0c10
Lya <lasvmateo@gmail.com>
2025-02-04 14:22:30 +0100

    link type correction

--- Account/inscription.php
+++ Account/inscription.php
@@ -12,10 +12,10 @@
 
         <div class="flex-nav"> <a href="index.php">Acceuil</a></div>
         <div class="flex-nav" id="FAQ"><p> FAQ </p>
-                <div class="flex-nav-Column"> <a href="../FAQ/FAQSport1.php">Sport1</a></div>
-                <div class="flex-nav-Column"> <a href="../FAQ/FAQSport2.php">Sport2</a></div>
-                <div class="flex-nav-Column"> <a href="../FAQ/FAQSport3.php">Sport3</a></div>
-                <div class="flex-nav-Column"> <a href="../FAQ/FAQSport4.php">Sport4</a></div>
+                <div class="flex-nav-Column"> <a href="../FAQ/FAQBask.php">Basket</a></div>
+                <div class="flex-nav-Column"> <a href="../FAQ/FAQFoot.php">Football</a></div>
+                <div class="flex-nav-Column"> <a href="../FAQ/FAQHand.php">Handball</a></div>
+                <div class="flex-nav-Column"> <a href="../FAQ/FAQVolle.php">Volley</a></div>
         </div>
         <div class="flex-nav" id="Account"><p> Compte </p> 
                 <div class="flex-nav-Column"> <a href="../Account/inscription.php">Inscription</a></div>
