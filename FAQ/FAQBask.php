6be7b59fc34739f9f4fcd2253a83bc92c17ba2d1
Lya <lasvmateo@gmail.com>
2025-02-11 11:54:31 +0100

    nav refactor php include

--- FAQ/FAQBask.php
+++ FAQ/FAQBask.php
@@ -9,21 +9,7 @@
 </head>
 <body>
     <div class="flex-title"> <h1>FAQ Basket</h1> </div>
-    <div class="flex-container">
-
-        <div class="flex-nav"> <a href="index.php">Acceuil</a></div>
-        <div class="flex-nav" id="FAQ"><p> FAQ </p>
-                <div class="flex-nav-Column"> <a href="../FAQ/FAQBask.php">Basket</a></div>
-                <div class="flex-nav-Column"> <a href="../FAQ/FAQFoot.php">Football</a></div>
-                <div class="flex-nav-Column"> <a href="../FAQ/FAQHand.php">Handball</a></div>
-                <div class="flex-nav-Column"> <a href="../FAQ/FAQVolle.php">Volley</a></div>
-        </div>
-        <div class="flex-nav" id="Account"><p> Compte </p> 
-                <div class="flex-nav-Column"> <a href="../Account/inscription.php">Inscription</a></div>
-                <div class="flex-nav-Column"> <a href="../Account/connection.php">Connection</a></div>
-                <div class="flex-nav-Column"> <a href="../Account/deconnection.php">Deconnection</a></div>
-        </div>
-    </div>
+    <?php include "../components/navbar.php"; ?>
     <div class="flex-page">
         <div class="flex-menu">
             <div class="select-sort"> trier</div> 
@@ -32,23 +18,7 @@
                 <div class="flex-question">
              
 
-
         </div>
-
-
-
-
-
-
-        
     </div>
-        
-
-
-
-
-
-
-
 </body>
 </html>
