b3c0624a2babbffa3129d85022ca200356b3585d
Lya <lasvmateo@gmail.com>
2025-02-11 12:09:17 +0100

    link corr

--- components/navbar.php
+++ components/navbar.php
@@ -1,11 +1,11 @@
 <div class="flex-container">
-    <div class="flex-nav"> <a href="/index.php">Acceuil</a></div>
+    <div class="flex-nav"> <a href="../index.php">Acceuil</a></div>
     <p class='flex-nav' style="text-align: center">nom prenom</p>
     <div class="flex-nav" id="FAQ"><p> FAQ </p>
-            <div class="flex-nav-Column"> <a href="/FAQ/FAQBask.php">Basket</a></div>
-            <div class="flex-nav-Column"> <a href="/FAQ/FAQFoot.php">Football</a></div>
-            <div class="flex-nav-Column"> <a href="/FAQ/FAQHand.php">Handball</a></div>
-            <div class="flex-nav-Column"> <a href="/FAQ/FAQVolle.php">Volley</a></div>
+            <div class="flex-nav-Column"> <a href="../FAQ/FAQBask.php">Basket</a></div>
+            <div class="flex-nav-Column"> <a href="../FAQ/FAQFoot.php">Football</a></div>
+            <div class="flex-nav-Column"> <a href="../FAQ/FAQHand.php">Handball</a></div>
+            <div class="flex-nav-Column"> <a href="../FAQ/FAQVolle.php">Volley</a></div>
     </div>
     <div class="flex-nav" id="Account"><p> Compte </p> 
             <div class="flex-nav-Column"> <a href="/Account/inscription.php">Inscription</a></div>
