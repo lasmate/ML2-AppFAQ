0de86d0dc1402d9fa65067d57d9d0bc09d7909aa
TheRates <raphael.tonon2805@gmail.com>
2025-02-11 15:30:27 +0100

    liens

--- FAQ/components/navbar.php
+++ FAQ/components/navbar.php
@@ -2,10 +2,10 @@
         <p class='flex-nav' style="text-align: center">nom prenom</p>
     <div class="flex-nav"> <a href="../index.php">Acceuil</a></div>
     <div class="flex-nav" id="FAQ"><p> FAQ </p>
-            <div class="flex-nav-Column"> <a href="../FAQBask.php">Basket</a></div>
-            <div class="flex-nav-Column"> <a href="../FAQFoot.php">Football</a></div>
-            <div class="flex-nav-Column"> <a href="../FAQHand.php">Handball</a></div>
-            <div class="flex-nav-Column"> <a href="../FAQVolle.php">Volley</a></div>
+            <div class="flex-nav-Column"> <a href="FAQBask.php">Basket</a></div>
+            <div class="flex-nav-Column"> <a href="FAQFoot.php">Football</a></div>
+            <div class="flex-nav-Column"> <a href="FAQHand.php">Handball</a></div>
+            <div class="flex-nav-Column"> <a href="FAQVolle.php">Volley</a></div>
     </div>
     <div class="flex-nav" id="Account"><p> Compte </p> 
             <div class="flex-nav-Column"> <a href="Account/inscription.php">Inscription</a></div>
