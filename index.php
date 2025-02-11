4cb47cffabb8067052efd62fe96380d25a3b78b6
TheRates <raphael.tonon2805@gmail.com>
2025-02-11 14:25:03 +0100

    tableaux accueil

--- index.php
+++ index.php
@@ -25,9 +25,17 @@
     </div>
 </div>
     <div class="flex-page">
-    
-        
+        <table>
+            <tr>
+                <td><a href="FAQ/FAQBask.php"><img src="images/basket.jpg" alt="Basket"></a></td>
+                <td><a href="FAQ/FAQFoot.php"><img src="images/foot.jpg" alt="Football"></a></td>
+            </tr>
+            <tr>
+                <td><a href="FAQ/FAQHand.php"><img src="images/hand.jpg" alt="Handball"></a></td>
+                <td><a href="FAQ/FAQVolle.php"><img src="images/volley.jpg" alt="Volley"></a></td>
+            </tr>
+        </table>
     </div>
-<?php include 'components/footer.php';?>
+<?php include 'FAQ/components/footer.php';?>
 </body>
 </html>
