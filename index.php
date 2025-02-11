b8b6f4f044c87c35a096ea8d3a7931e043c55966
TheRates <raphael.tonon2805@gmail.com>
2025-02-11 11:19:05 +0100

    mise a jour accueil

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+    <link rel="stylesheet" href="../style/main.css">
+</head>
+<body>
+    <div class="flex-title"> <h1>Accueil</h1> </div>
+    <div class="flex-container">
+
+        <div class="flex-nav"> <a href="index.php">Acceuil</a></div>
+        <div class="flex-nav" id="FAQ"><p> FAQ </p>
+                <div class="flex-nav-Column"> <a href="FAQ/FAQBask.php">Basket</a></div>
+                <div class="flex-nav-Column"> <a href="FAQ/FAQFoot.php">Football</a></div>
+                <div class="flex-nav-Column"> <a href="FAQ/FAQHand.php">Handball</a></div>
+                <div class="flex-nav-Column"> <a href="FAQ/FAQVolle.php">Volley</a></div>
+        </div>
+        <div class="flex-nav" id="Account"><p> Compte </p> 
+                <div class="flex-nav-Column"> <a href="Account/inscription.php">Inscription</a></div>
+                <div class="flex-nav-Column"> <a href="Account/connection.php">Connection</a></div>
+                <div class="flex-nav-Column"> <a href="Account/deconnection.php">Deconnection</a></div>
+        </div>
+    </div>
+    <div class="flex-page">
+
+
+
+
+
+
+
+
+        
+    </div>
+        
+
+
+
+
+
+
+
+</body>
+</html>
