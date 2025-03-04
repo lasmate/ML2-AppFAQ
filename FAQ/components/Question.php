b9e0d9a33280693b3638618c96e85fb2146f2b94
Lya <lasvmateo@gmail.com>
2025-03-04 15:24:09 +0100

    refactor: replace include with require_once for header and navbar components

--- FAQ/components/Question.php
+++ FAQ/components/Question.php
@@ -1,26 +1,23 @@
 <?php 
-    $questions = array("Question 1", "Question 2", "Question 3", "Question 4", "Question 5");
+    $questions = array("Quand sera le prochain tournis?", "y'a-il des maillot fourni par la", "Question 3", "Question 4", "Question 5");
     $answers = array("Answer 1", "Answer 2", "Answer 3", "Answer 4", "Answer 5");
     $theme = array("theme 1", "theme 2", "theme 3", "theme 4", "theme 5");
     echo"<div>";
-    foreach ($theme as $theme) {
+    foreach ($theme as $singleTheme) {
         echo "<div class='flex-question'>". $theme;
         foreach ($questions as $question) {//$questions is an array
             echo "<div class='flex-question'><div class='question'>" . $question . "</div>";
-            echo "<div class='answer'>" . "Answer to " . $question . "</div>";
-            echo "<form>";
+            $index = array_search($question, $questions);
+            echo "<div class='answer'>{$answers[$index]}</div>";
+
+            echo "<form action='FAQModification/msgmodif.php' method='post'>";
+            echo "<input type='hidden' name='question' value='$question'>";
+ echo "";
             echo "<button type='submit' id='question_$question' class='button-add'>Réponse</button>";
             echo "</form></div>";
         }
         echo "</div>";
     }
-        
-
-
-
-
-
-
 ?>
 <style>
     .flex-question {
@@ -37,7 +34,6 @@
         font-style: italic;
     }
     .button-add {
-        color: white;
           background-color: transparent;
           font-family: Quicksand, sans-serif;
           color: black;
@@ -45,6 +41,7 @@
           display: block;
           margin: 10px 0px;
           text-decoration: none;
+          padding: 5px;
           padding: 5px;
     }
 </style>
