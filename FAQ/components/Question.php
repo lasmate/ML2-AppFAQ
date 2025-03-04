3902c01fb5099d5fa2c1bf73ee90aae5ba904b76
TheRates <raphael.tonon2805@gmail.com>
2025-03-04 11:43:20 +0100

    theme

--- FAQ/components/Question.php
+++ FAQ/components/Question.php
@@ -1,11 +1,17 @@
 <?php 
     $questions = array("Question 1", "Question 2", "Question 3", "Question 4", "Question 5");
     $answers = array("Answer 1", "Answer 2", "Answer 3", "Answer 4", "Answer 5");
-    echo"<div class='flex-question'>";
+    $theme = array("theme 1", "theme 2", "theme 3", "theme 4", "theme 5");
+    echo"<div>";
+    foreach ($theme as $theme) {
+        echo "<div class='flex-question'>". $theme;
         foreach ($questions as $question) {//$questions is an array
-            echo "<div class='question'>" . $question . "</div>";
-            echo "<div class='answer'>" . "Answer to " . $question . "</div>";
+            echo "<div class='flex-question'><div class='question'>" . $question . "</div>";
+            echo "<div class='answer'>" . "Answer to " . $question . "</div></div>";
         }
+        echo "</div>";
+    }
+        
 
 
 
