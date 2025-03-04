d619c1e0fa861c4ef15b27e65587cad7988d7976
Lya <lasvmateo@gmail.com>
2025-03-04 16:05:08 +0100

    .

--- FAQ/components/Question.php
+++ FAQ/components/Question.php
@@ -1,27 +1,26 @@
 <?php 
     $questions = array("Quand sera le prochain tournis?", "y'a-il des maillot fourni par la", "Question 3", "Question 4", "Question 5");
     $answers = array("Answer 1", "Answer 2", "Answer 3", "Answer 4", "Answer 5");
-    $description = array("Description 1", "Description 2", "Description 3", "Description 4", "Description 5");
+    $description = array("Jaimerais savoir quand ce passera le procain tournois, j'ai vu passer un mail qui disais que ca serai la semaine prochaine mais notre entraineur a dit que cetait dans 2 mois ", "Description 2", "Description 3", "Description 4", "Description 5");
     $theme = array("theme 1", "theme 2", "theme 3", "theme 4", "theme 5");
     echo"<div>";
     foreach ($theme as $singleTheme) {
         echo "<div class='flex-question'>". $theme;
-        foreach ($questions as $question) {//$questions is an array
-            echo "<div class='flex-question'><div class='question'>" . $question . "</div>";
-            echo "<div class='flex-question'><div class='description'>" . "Description to " . $question . "</div></div>";
-            echo "<form>";
-            $index = array_search($question, $questions);
-            echo "<div class='answer'>{$answers[$index]}</div>";
-
-            echo "<form action='FAQModification/msgmodif.php' method='post'>";
-            echo "<input type='hidden' name='question' value='$question'>";
- echo "";
-            echo "<button type='submit' id='question_$question' class='button-add'>Réponse</button>";
-            echo "</form>";
-            echo "<div class='flex-question'><div class='answer'>" . "Answer to " . $question . "</div></div></div>";
+        for ($i = 0; $i < count($questions); $i++)
+        {
+            if ($theme[$i] === $singleTheme) {
+                echo "<form class='question-form'>";
+                echo "<div class='question'>" . $questions[$i] . "</div>";
+                echo "<div class='description'>" . $description[$i] . "</div>";
+                echo "<button type='button' class='button-add'>Answer</button>";
+                echo "<div class='answer'>" . $answers[$i] . "</div>";
+                echo "</form>";
+            }
         }
-        echo "</div>";
-    }
+
+        }
+    echo "</div>";
+    
 ?>
 <style>
     .flex-question {
