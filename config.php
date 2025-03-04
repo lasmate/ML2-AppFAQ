fec120f0a85b93f24565f6f26f1d1d2966beabe6
Lya <lasvmateo@gmail.com>
2025-03-04 14:41:45 +0100

    feat: add configuration file for project paths in XAMPP environment

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+// Define a constant for XAMPP's root directory
+define('XAMPP_ROOT', '/opt/lampp/');
+
+// Document root path
+define('DOC_ROOT', '/opt/lampp/htdocs/');
+
+// Current project path
+define('PROJECT_PATH', DOC_ROOT . 'Projects/ML2-AppFAQ/');
+?>
