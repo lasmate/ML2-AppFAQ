119e4cb977f8c4b04130477a5ee400514982ad63
Lya <lasvmateo@gmail.com>
2025-03-28 13:44:34 +0100

    feat: ajout de logique d'affichage des boutons de modification et suppression de messages

--- FAQ/components/Question.php
+++ FAQ/components/Question.php
@@ -13,28 +13,24 @@ echo "<div>";
 for ($i = 0; $i < count($faqdata); $i++) {
     echo "<div class='flex-question'>";
     echo "<form class='question-form' method='post'>";
-    echo "<div class='question'>" . htmlspecialchars($questions[$i], ENT_QUOTES) . "<br><span style='font-weight:150;font-size:0.9em;'>" . htmlspecialchars($Qdate[$i], ENT_QUOTES) . "</span></div>";
+    echo "<div class='question'>" . htmlspecialchars_decode($questions[$i], ENT_QUOTES) . "<br><span style='font-weight:150;font-size:0.9em;'>" . htmlspecialchars($Qdate[$i], ENT_QUOTES) . "</span></div>";
     echo "<p>Utilisateur : " . htmlspecialchars($IDuser[$i], ENT_QUOTES) . "</p>";
     if (empty($reponses[$i])) { // Gestion des erreurs s'il n'y a pas de réponse
         echo "<input type='hidden' name='reponse_message' value='Aucune réponse disponible.'>";
     } else {
-        echo "<div class='reponse'>" . htmlspecialchars($reponses[$i], ENT_QUOTES) . "<br><span style='font-weight:150;font-size:0.7em;'>" . htmlspecialchars($Adate[$i], ENT_QUOTES) . "</span></div>";
+        echo "<div class='reponse'>" . htmlspecialchars_decode($reponses[$i], ENT_QUOTES) . "<br><span style='font-weight:150;font-size:0.7em;'>" . htmlspecialchars($Adate[$i], ENT_QUOTES) . "</span></div>";
     }
 
     echo "<input type='hidden' name='IDquestion' value='" . htmlspecialchars($IDQ[$i], ENT_QUOTES) . "'>";// ID de la question
     echo "<input type='hidden' name='question_id' value='" . htmlspecialchars($IDfaq[$i], ENT_QUOTES) . "'>"; // ID pour quelle ligue est la question
-    echo "<input type='hidden' name='question' value='" . htmlspecialchars($questions[$i], ENT_QUOTES) . "'>";// question
-    if (!empty($reponses[$i])) {
-        echo "<input type='hidden' name='reponse' value='" . htmlspecialchars($reponses[$i], ENT_QUOTES) . "'>"; // reponse
-    } else {
-        echo "<input type='hidden' name='reponse' value='Aucune réponse disponible.'>"; // Default message for empty response
-    }
     echo "<input type='hidden' name='id_user' value='" . htmlspecialchars($IDuser[$i], ENT_QUOTES) . "'>"; // ID de l'utilisateur
     echo "<input type='hidden' name='dat_question' value='" . htmlspecialchars($Qdate[$i], ENT_QUOTES) . "'>"; // date de la question
     echo "<input type='hidden' name='dat_reponse' value='" . htmlspecialchars($Adate[$i], ENT_QUOTES) . "'>"; // date de la réponse
-    
-echo "<button type='button' class='button-add modify-button' data-id='" . htmlspecialchars($IDQ[$i], ENT_QUOTES) . "'>Repondre/Modifier</button>";
-    echo "<button type='button' class='button-add delete-button' data-id='" . htmlspecialchars($IDQ[$i], ENT_QUOTES) . "'>Supprimer</button>";
+    if($_SESSION["id_usertype"]    !='3'){
+        echo "<button type='button' class='button-add modify-button' data-id='" . htmlspecialchars($IDQ[$i], ENT_QUOTES) . "'>Repondre/Modifier</button>";
+        echo "<button type='button' class='button-add delete-button' data-id='" . htmlspecialchars($IDQ[$i], ENT_QUOTES) . "'>Supprimer</button>";
+    }
+  
     echo "</form>";
     echo "</div>";
 }
